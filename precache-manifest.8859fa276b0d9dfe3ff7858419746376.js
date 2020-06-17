ff02c14d21b86b8ccae7c63d86f85e41e4406bc9
Saskuatch <asmithamongus@gmail.com>
2020-06-17 12:48:46 -0700

    really the final commit with build run and new readme

---
+++ precache-manifest.8859fa276b0d9dfe3ff7858419746376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c82938524c78f443b70753fe65ca491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b6b99b37bc8cfc9bf5",
+    "url": "/static/js/2.c356fb66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c356fb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a35ba31158e5db676f",
+    "url": "/static/js/main.a053db4b.chunk.js"
+  },
+  {
+    "revision": "c0f6cd6586f56a27692a",
+    "url": "/static/js/runtime-main.8d52288f.js"
+  }
+]);
