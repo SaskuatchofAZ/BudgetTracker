2a1d8bbcdfa988b55e5c9ed1c6c6793cc7406d87
Saskuatch <asmithamongus@gmail.com>
2020-06-16 18:16:03 -0700

    initaial commit with react app

---
+++ budget-tracker/src/components/Form/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function Form() {
+  const [amounts, setAmounts] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
+
+  return (
+    
+  )
+}
+
+export default Form;
